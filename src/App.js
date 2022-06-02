344e2aea77c1760288447332ed90db819c59a8c9
MaMy89 <99359203+MaMy89@users.noreply.github.com>
2022-06-02 10:44:54 +0200

    Added link to GitHub

--- src/App.js
+++ src/App.js
@@ -2,5 +2,15 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project is coded by Mari Myhrer and is open-sourced on{" "}
+        <a href="https://github.com/MaMy89/react-weather-app" target="_blank">
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
