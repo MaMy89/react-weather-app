44c78a3b3679f732d7a99be80e9ee5af8cd23b1a
MaMy89 <99359203+MaMy89@users.noreply.github.com>
2022-06-03 09:16:39 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project is coded by Mari Myhrer and is open-sourced on{" "}
-        <a href="https://github.com/MaMy89/react-weather-app" target="_blank">
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <button className="btn btn-primary">Hello</button>
+        <footer>
+          This project is coded by Mari Myhrer and is open-sourced on{" "}
+          <a href="https://github.com/MaMy89/react-weather-app" target="_blank">
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
