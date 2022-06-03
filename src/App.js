cd2c0c8c72a0dc523c930bde4230dfd7a4e1349d
MaMy89 <99359203+MaMy89@users.noreply.github.com>
2022-06-03 21:23:50 +0200

    more bugs fixed

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ export default function App() {
           <a
             href="https://github.com/MaMy89/react-weather-app"
             target="_blank"
-            rel="noreferrer"
+            rel="noopener noreferrer"
           >
             GitHub
           </a>
